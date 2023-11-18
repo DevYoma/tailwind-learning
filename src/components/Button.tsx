ef86a5ca6742d5d29deebee4a39ae13cdbdcb0ef
Ogheneyoma <Lawrenceyoma@gmail.com>
2023-11-18 12:10:48 +0100

    style: finished desigining landing page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,33 @@
 type ButtonProps = {
     label: string;
     iconURL?: string;
+    backgroundColor?: React.CSSProperties | string;
+    textColor?: React.CSSProperties | string;
+    borderColor?: React.CSSProperties | string;
+    fullWidth?: React.CSSProperties | string;
 }
 
-const Button = ({ label, iconURL }: ButtonProps) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor, fullWidth }: ButtonProps) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}`
+        : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && 'w-full'}"}
+    `}>
         {label}
 
         {/* img */}
-        <img 
+        {iconURL && <img 
             src={iconURL} 
             alt="arrow right icon"
             className="ml-2 rounded-full w-5 h-5" 
-        />
+        />}
+        
     </button>
   )
 }
 
 export default Button
+
+// observe well, this component.
--- src/sections/SpecialOffer.tsx
+++ src/sections/SpecialOffer.tsx
@@ -1,6 +1,41 @@
+import { offer } from '../assets/images';
+import Button from '../components/Button';
+import { arrowRight } from '../assets/icons';
+
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section className="flex justify-center items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img 
+          src={offer} 
+          width={773}
+          height={687}
+          className='object-contain w-full'
+        />
+      </div>
+
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          <span className="text-coral-red">Special</span> Offer
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">Explore our exclusive limited-time offer! Elevate your shoe game with discounts on premium styles. Don't miss out on the chance to step into luxury at unbeatable prices. Shop now!</p>
+
+        <p className="mt-6 lg:max-w-lg info-text">Indulge in savings and step out in style. Our special offer brings you trendy footwear at irresistible prices. Upgrade your wardrobe with high-quality shoes without breaking the bank. Limited stock, act fast!</p>
+
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button 
+            label="Shop now" 
+            iconURL={arrowRight}
+          />
+          <Button 
+            label="Learn more"
+            backgroundColor="bg-white"
+            borderColor="border-slate-gray"
+            textColor="text-slate-gray"
+          />
+        </div>
+      </div>
+    </section>
   )
 }
 
